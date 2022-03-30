f383d083dd6c681e9141b1e7b4bc0eca709ed4d5
Aniket Kaimal <aniket1995@yahoo.co.in>
2022-03-30 20:19:58 +0530

    added my solution.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,9 +1,16 @@
-def caesar_cipher(string, key)
-    array1 = string.chars.map{|x| x.ord}
-    letter_check = array1.map{|number| (number >= 97 && number <= 122) || (number >= 65 && number <= 90)? number : number.chr}
-    wrap_array = letter_check.map{ |number| number.is_a?(Numeric) ? number + key : number}
-    final_array = wrap_array.map { |number| number.is_a?(Numeric) && number > 122 || (number.is_a?(Numeric) && number > 90 && number <97) ? number - 26 : number}
-   p x = final_array.map{ |c| c.chr}.join('')
+def caesar_cipher(string, shift_factor)
+  str = string.split('')
+  str1 = str.map { |word| word.downcase.ord }
+  str2 = str1.map do |word|
+    if (0..47).include?(word)
+      word
+    else
+      ((word + shift_factor - 97) % 26) + 97
+    end
+  end
+  str3 = str2.map(&:chr)
+  puts str3.join
 end
 
-caesar_cipher("What a string!", 5)
+caesar_cipher('Aniket Unnikrishnan Kaimal', -5)
+caesar_cipher('vidfzo piidfmdncivi fvdhvg', 5)
